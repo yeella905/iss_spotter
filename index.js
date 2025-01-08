9e61bce388cb58225015b7617c55325a850c25bd
yeella905 <yeella905@gmail.com>
2025-01-08 01:17:05 -0500

    fetching coordinates

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 // index.js
 
 
-const { fetchMyIP } = require('./iss');
+// const { fetchMyIP } = require('./iss');
 
-fetchMyIP((error, ip) => {
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+const { fetchCoordsByIP } = require('./iss');
+
+fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned IP:' , ip);
+  console.log('It worked! Returned coordinates:' , coordinates);
 });
--- iss.js
+++ iss.js
@@ -9,19 +9,25 @@ const needle = require('needle');
  *   - The IP address as a string (null if error). Example: "162.245.144.188"
  */
 
-
-const fetchMyIP = function(callback) { 
-    // use request to fetch IP address from JSON API
-  needle.get('https://api.ipify.org?format=json', (error, response, body) => {
-    if (error) return callback(error, null);
-
-    if (response.statusCode !== 200) {
-      callback(Error(`Status Code ${response.statusCode} when fetching IP: ${body}`), null);
-      return;
-    }
-
-    const ip = body.ip;
-    callback(null, ip);
-  });
-};
-  module.exports = { fetchMyIP };
+const fetchCoordsByIP = function(ip, callback) {
+    needle.get(`http://ipwho.is/${ip}`, (error, response, body) => {
+  
+      if (error) {
+        callback(error, null);
+        return;
+      }
+  
+      if (!body.success) {
+        const message = `Success status was ${body.success}. Server message says: ${body.message} when fetching for IP ${body.ip}`;
+        callback(Error(message), null);
+        return;
+      } 
+  
+      const latitude = body.latitude
+      const longitude = body.longitude
+      callback(null, {latitude, longitude});
+    });
+  };
+  
+  // Don't need to export the other function since we are not testing it right now.
+  module.exports = { fetchCoordsByIP };
